dc5e0496e6af1866ea6d8c79d4a2866814884fcc
Joseph Phillips <josephephillips27@gmail.com>
2013-12-23 23:06:21 -0500

    Completion of 2.4

--- Handle_Form.php
+++ Handle_Form.php
@@ -5,38 +5,72 @@
 <head>
 	<meta http-equiv-"Content=Type"
 	content="text/html"; charset="utf-8"/>
-	<title>Form Feedback </title>
+	<title>Form Feedback</title>
+	<style type="text/css" title="text/css" media="all">
+	.error{
+		font-weight: bold;
+		color: #C00;
+	}
+	</style>
 </head>
 <body>
 <?php
 
-# Script 2.3 handle_form.php
+# Script 2.4 handle_form.php
 
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$comments = $_REQUEST['comments'];
+//Validate the name:
+	if (!empty($_REQUEST['name'])) {
+		$name = $_REQUEST['name'];
+	} else {
+		$name = null;
+		echo '<p class ="error">You forgot to enter your name!</p>';
+	}
 
-//Create the $gender variable
-if (isset($_REQUEST['gender'])) {
-	$gender = $_REQUEST['gender'];
+//Validate the email:	
+if (!empty($_REQUEST['email'])) {
+	$email = $_REQUEST['email'];
 } else {
-	$gender = NULL;
+	$email = null;
+	echo "<p class ='error'>You forgot to enter your email address!</p>";
 }
 
-echo "<p> Thank You, <b> $name </b>, 
+//Validate the comments
+if ($_REQUEST['comments']) {
+	$comments = $_REQUEST['comments'];
+} else {
+	$comments = null;
+	echo "<p class ='error'>You forgot to enter your comments!</p>";
+}
+
+//Validate the gender:
+if (isset($_REQUEST['gender'])) {
+
+	$gender = $_REQUEST['gender'];
+
+	if ($gender =='M') {
+		echo "<p><b>Good day, Sir!</b></p>";
+	} elseif ($gender == 'F') {
+		echo "<p><b>Good day, Madam!</b></p>";
+	} 	else {
+		echo "<p><b>Gender should be either 'M' or 'F'!</b></p>";
+	}
+
+} else {
+	
+	$gender = NULL;
+	echo "<p class='error'>You forgot to select your gender!</p>";
+}
+if ($name && $email && $gender && $comments) {
+	
+	echo "<p> Thank You, <b> $name </b>, 
 		for the following comments:
 		<br /> <tt>$comments</tt></p>
 		<p> We will reply to you at <i> $email
 		</i></p>\n";
-
-//Print a message based upon the gender value
-if ($gender =='M') {
-	echo "<p><b>Good day, Sir!</b></p>";
-} elseif ($gender == 'F') {
-	echo "<p><b>Good day, Madam!</b></p>";
 } else {
-	echo "<p><b> You forgot to enter your gender!</b></p>";
+	echo "<p class='error'>Please go back and fill out the form again.</p>";
 }
+
 
 ?>
 </body>
