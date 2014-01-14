9ddd3b047275db2f9e8e269d2bd70b8e2fff24e9
Joseph Phillips <josephephillips27@gmail.com>
2014-01-13 22:02:39 -0500

    worked through 3.7 - functions

--- Chapter 3/index.php
+++ Chapter 3/index.php
@@ -1,15 +1,28 @@
 <?php 
-#Script 3.4 - index.php 
+#Script 3.7 - index.php #2
+//This function outputs theoretical HTML
+//for adding ads to a Web page
+function  create_ad() {
+	echo '<p class="ad">This is an annoying ad!This is an annoying ad!This is an annoying ad!This is an annoying ad!</p>';
+} //End of function definition
+
 $page_title = "Welcome to this site!";
 include ('include/header.html');
+create_ad();
 ?>
 
-			<h1>Content Header</h1>
+<h1>Content Header</h1>
 			
-				<p>This is where the page-specific content goes. This section, and the corresponding header, will change from one page to the next.</p>
+	<p>This is where the page-specific content goes. This section, and the corresponding header, 
+		will change from one page to the next.</p>
+	<p>Volutpat at varius sed sollicitudin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam.
+		 Lorem ipsum sit amet dolore. Nulla facilisi. Sed tortor. Aenean felis. Quisque eros. Cras lobortis commodo metus. 
+		 Vestibulum vel purus. In eget odio in sapien adipiscing blandit. Quisque augue tortor, facilisis sit amet, aliquam,
+		 suscipit vitae, cursus sed, arcu lorem ipsum dolor sit amet.</p>
 
-				<p>Volutpat at varius sed sollicitudin et, arcu. Vivamus viverra. Nullam turpis. Vestibulum sed etiam. Lorem ipsum sit amet dolore. Nulla facilisi. Sed tortor. Aenean felis. Quisque eros. Cras lobortis commodo metus. Vestibulum vel purus. In eget odio in sapien adipiscing blandit. Quisque augue tortor, facilisis sit amet, aliquam, suscipit vitae, cursus sed, arcu lorem ipsum dolor sit amet.</p>
+<?php 
+create_ad();
 
-<?
-include('include/footer.html');
-?>				
+include ('include/footer.html'); 
+
+?>
