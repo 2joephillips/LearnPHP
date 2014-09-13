0c322d6244db08f5b1bf8bfc4a59ec11136abc2f
Joseph Phillips <josephephillips27@gmail.com>
2014-09-13 15:42:48 -0400

    started ECommerce site

---
+++ e-commerce/app/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+class DatabaseSeeder extends Seeder {
+
+	protected $faker;
+
+	public function getFaker()
+	{
+		if(empty($this->faker))
+		{
+			$this->faker = Faker\Factory::create();
+		}
+
+		return $this->faker;
+	}
+	
+	public function run()
+	{
+		Eloquent::unguard();
+
+		$this->call('AccountTableSeeder');
+	}
+
+}
